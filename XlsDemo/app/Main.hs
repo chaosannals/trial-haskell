136aa2218e8aebb9852894ba76ef8b96f71cbbfb
chaosannals <chaosannals@outlook.com>
2024-01-16 15:20:11 +0800

    save

--- XlsDemo/app/Main.hs
+++ XlsDemo/app/Main.hs
@@ -1,6 +1,7 @@
 module Main where
 
 -- 只支持 Windows ，但是现在高版本 ghcup 都是用 linux 环境，windows 下是 mingw32 封装的。
+-- 而且这个库封装 libxls ，libxls 是不支持图片的。
 import Data.List (intercalate)
 import Data.Xls (decodeXlsIO)
 
