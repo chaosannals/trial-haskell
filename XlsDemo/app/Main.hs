57f433dfda83d25cf6740631e47d888c58bcac35
chaosannals <chaosannals@outlook.com>
2024-01-16 15:18:27 +0800

    save

---
+++ XlsDemo/app/Main.hs
@@ -0,0 +1,13 @@
+module Main where
+
+-- 只支持 Windows ，但是现在高版本 ghcup 都是用 linux 环境，windows 下是 mingw32 封装的。
+import Data.List (intercalate)
+import Data.Xls (decodeXlsIO)
+
+xlsToCSV :: String -> IO ()
+xlsToCSV file = do
+    worksheets <- decodeXlsIO file
+    mapM_ (mapM_ (putStrLn . intercalate ",")) worksheets
+
+main :: IO ()
+main = putStrLn "Hello, Haskell!"
