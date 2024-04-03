cd70e7567795c37fa36088900cd6c34ad4d80e42
chaosannals <chaosannals@outlook.com>
2024-04-03 10:34:57 +0800

    save

--- BeamDemo/SelectDemo/Main.hs
+++ BeamDemo/SelectDemo/Main.hs
@@ -8,6 +8,9 @@ import Database.SQLite.Simple
 import SqliteDemo.Simple
 import Control.Applicative
 import Control.Monad (forM_)
+import Database.SQLite.Simple (close)
+import Database.Beam (orderBy_)
+import SqliteDemo.Simple (MyDbT(myUsers))
 
 data UserRow = UserRow Int String String String String deriving(Show)
 instance FromRow UserRow where
@@ -26,14 +29,25 @@ main = do
   
   -- 不打印 SQL
   users <- runBeamSqlite conn $
-    runSelectReturningList $ select $ all_ (myUsers myDb)
+    runSelectReturningList $
+    select $
+    orderBy_ (desc_ . userId) $
+    all_ (myUsers myDb)
   
   -- 打印SQL
   users2 <- runBeamSqliteDebug putStrLn conn $
-    runSelectReturningList $ select $
-      limit_ 2 $
-      filter_ (\u -> (userEmail u `like_` "%@%") &&. (userId u >=. 0)) $
-      all_ (myUsers myDb)
+    runSelectReturningList $
+    select $
+    limit_ 2 $
+    offset_ 1 $
+    filter_ (\u -> (userEmail u `like_` "%@%") &&. (userId u >=. 0)) $
+    orderBy_ (\e ->
+        (
+          asc_ (userId e),
+          desc_ (userFirstName e)
+        )
+      ) $
+    all_ (myUsers myDb)
 
   putStrLn "users: "
   forM_ users $ \row ->
@@ -43,3 +57,4 @@ main = do
   forM_ users2 $ \row ->
     putStrLn $ show row
   putStrLn "end."
+  close conn
