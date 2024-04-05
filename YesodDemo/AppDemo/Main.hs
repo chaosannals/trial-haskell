ce6554a7353bfc660ad2f8669bf0f9dd0c396c73
chaosannals <chaosannals@outlook.com>
2024-04-05 11:43:38 +0800

    save

--- YesodDemo/AppDemo/Main.hs
+++ YesodDemo/AppDemo/Main.hs
@@ -1,8 +1,21 @@
-{-# LANGUAGE OverloadedStrings, QuasiQuotes, TemplateHaskell, TypeFamilies #-}
+{-# LANGUAGE OverloadedStrings #-}
+{-# LANGUAGE MultiParamTypeClasses #-}
+{-# LANGUAGE QuasiQuotes           #-}
+{-# LANGUAGE TemplateHaskell       #-}
+{-# LANGUAGE TypeFamilies          #-}
 
 module Main where
 
-import Yesod
+import Yesod.Core
+import Routes
+
+instance Yesod App
+instance YesodDispatch App
+
+getHomeR :: Handler String
+getHomeR = return "Hello"
 
 main :: IO ()
-main = putStrLn "Hello, Haskell!"
+main = do
+    putStrLn "start"
+    warp 44444 App
---
+++ YesodDemo/Readme.md
@@ -0,0 +1,7 @@
+# yesod demo
+
+```bash
+# 安装命令工具
+cabal install yesod-bin
+cabal install yesod-platform
+```
